5cb469fa16f9922c8c0cad6085cead46b05f5faf
Jeff Deutsch <deutsch.jeff.a@gmail.com>
2023-01-16 11:45:42 -0500

    feat: update fetch location

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from the server for the AI response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-ai-st01.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
